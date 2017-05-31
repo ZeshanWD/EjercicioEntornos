9281017b699d6fe1f360ec952eb71296918ff050
ZeshanDeveloper <zeeshanpaki@hotmail.com>
2017-05-31 13:06:22 +0200

    He añadido comentarios y otro println

--- ProyectoGitEntornos/src/Hola.java
+++ ProyectoGitEntornos/src/Hola.java
@@ -1,8 +1,12 @@
 
 public class Hola {
 
-	
+	/**
+	 * Clase Main
+	 * @param args
+	 */
 	public static void main (String args[]){
+		System.out.println("Hello, My name is Zeshan");
 		System.out.println("Hola");
 	}
 }
